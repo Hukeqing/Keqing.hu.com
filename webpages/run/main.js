cade09f8bafff276f36706cedf0047fe38e2f799
HuKeqing <keqing.hu@icloud.com>
2019-11-09 11:33:07 +0800

    run

---
+++ webpages/run/main.js
@@ -0,0 +1,10 @@
+function getclick(id) {
+    document.getElementById("normal").style.opacity = "0";
+    document.getElementById("select1").style.opacity = "0";
+    document.getElementById("select2").style.opacity = "0";
+    document.getElementById("select3").style.opacity = "0";
+    document.getElementById("select4").style.opacity = "0";
+    document.getElementById("select5").style.opacity = "0";
+    document.getElementById("select6").style.opacity = "0";
+    document.getElementById(id).style.opacity = "1";
+}
